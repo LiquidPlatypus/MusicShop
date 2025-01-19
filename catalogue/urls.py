8ad360b5685dcd7d62d3d60dcb355b93c2638bfa
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2025-01-19 22:48:34 +0100

    essaie d'afficher un listing
    marche pas

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import articles_listing
+from .views import listing
 
 urlpatterns = [
-	path('', articles_listing, name='liste_articles'),
+	path('', listing, name='listing'),
+	path('<int:article_id>/', listing, name='details')
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
-def articles_listing(request):
-    articles = Article.objects.all()
-    return render(request, 'catalogue/liste_articles.html', {'articles': articles})
+def listing(request):
+	latest_articles_list = Article.objects.order_by('-date_ajout')[:5]
+	context = {'latest_articles_list': latest_articles_list}
+	return render(request, 'catalogue/listing.html', context)
+
+def details(request, article_id):
+	article = get_object_or_404(Article, pk=article_id)
+	return render(request, 'catalogue/details.html', {'article': article})
