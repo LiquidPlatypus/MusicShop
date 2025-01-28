ae805827dbc7e0a2ad91236296a445e48d5f6c19
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2025-01-28 10:05:20 +0100

    new models + test

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,14 +3,15 @@ from django.db import models
 from django.utils import timezone
 
 class Article(models.Model):
-    nom = models.CharField(max_length=255)
-    description = models.TextField()
-    prix = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
-    date_ajout = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.nom
-    
-    def was_published_recently(self):
-        return self.date_ajout >= timezone.now() - datetime.timedelta(days=1)
+	nom = models.CharField(max_length=255)
+	description = models.TextField()
+	prix = models.DecimalField(max_digits=10, decimal_places=2)
+	stock = models.IntegerField()
+	date_ajout = models.DateTimeField(auto_now_add=True)
+	
+	def __str__(self):
+		return self.nom
+	
+	def was_published_recently(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.date_ajout <= now
--- catalogue/tests.py
+++ catalogue/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Article
+
+class ArticleModelTests(TestCase):
+	
+	def test_was_published_recently_with_future_article(self):
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_article = Article(date_ajout=time)
+		self.assertIs(future_article.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_article(self):
+		time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+		old_article = Article(date_ajout=time)
+		self.assertIs(old_article.was_published_recently(), False)
+		
+	def test_was_published_recently_with_recent_article(self):
+		time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+		recent_article = Article(date_ajout=time)
+		self.assertIs(recent_article.was_published_recently(), True)
